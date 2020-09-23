3ec59109d96d26c1736c10c537a7259f834b2757
rizkyalviandra <rizkyalviandra@gmail.com>
2020-09-23 23:20:16 +0700

    add tmux,zsh,zsh custom plugin

---
+++ react.plugin.zsh
@@ -0,0 +1,9 @@
+# react new app command
+alias cra='create-react-app'
+alias crats='create-react-app --template typescript'
+
+# react command
+alias rs='npx react-scripts start'
+alias rb='npx react-scripts --max_old_space_size=8192 build'
+alias rt='npx react-scripts test'
+alias re='npx react-scripts eject'
